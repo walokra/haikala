dfb7a17d37d60984a402b8f09131a462c24fc875
Marko Wallin <mtw@iki.fi>
2014-10-12 18:25:26 +0300

    Whitespace

--- translations/harbour-haikala-fi.ts
+++ translations/harbour-haikala-fi.ts
@@ -4,347 +4,347 @@
 <context>
     <name>AboutPage</name>
     <message>
-	<source>About Haikala</source>
-	<translation>Tietoja Haikalasta</translation>
+        <source>About Haikala</source>
+        <translation>Tietoja Haikalasta</translation>
     </message>
     <message>
-	<source>Haikala is a news reader for</source>
-	<translation>Haikala on uutislukija</translation>
+        <source>Haikala is a news reader for</source>
+        <translation>Haikala on uutislukija</translation>
     </message>
     <message>
-	<source>Version</source>
-	<translation>Versio</translation>
+        <source>Version</source>
+        <translation>Versio</translation>
     </message>
     <message>
-	<source>Developed by</source>
-	<translation>Kehittäjä</translation>
+        <source>Developed by</source>
+        <translation>Kehittäjä</translation>
     </message>
     <message>
-	<source>Bug reports</source>
-	<translation>Virheraportit</translation>
+        <source>Bug reports</source>
+        <translation>Virheraportit</translation>
     </message>
     <message>
-	<source>Powered by</source>
-	<translation>Voimanlähteenä</translation>
+        <source>Powered by</source>
+        <translation>Voimanlähteenä</translation>
     </message>
     <message>
-	<source>news portal&apos;s feeds. Haikala is open source software and licensed under the terms of the MIT license.</source>
-	<translation>uutisportaalin syötteille. Haikala on avoimen lähdekoodin ohjelmisto ja lisensoitu MIT-lisenssillä.</translation>
+        <source>news portal&apos;s feeds. Haikala is open source software and licensed under the terms of the MIT license.</source>
+        <translation>uutisportaalin syötteille. Haikala on avoimen lähdekoodin ohjelmisto ja lisensoitu MIT-lisenssillä.</translation>
     </message>
     <message>
-	<source>Changelog</source>
-	<translation>Muutokset</translation>
+        <source>Changelog</source>
+        <translation>Muutokset</translation>
     </message>
 </context>
 <context>
     <name>ChangelogDialog</name>
     <message>
-	<source>Changelog</source>
-	<translation>Muutokset</translation>
+        <source>Changelog</source>
+        <translation>Muutokset</translation>
     </message>
     <message>
-	<source>Version</source>
-	<translation>Versio</translation>
+        <source>Version</source>
+        <translation>Versio</translation>
     </message>
     <message>
-	<source>Close changelog</source>
-	<translation>Sulje muutostiedote</translation>
+        <source>Close changelog</source>
+        <translation>Sulje muutostiedote</translation>
     </message>
     <message>
-	<source>Search functionality.</source>
-	<translation>Haku-toiminto.</translation>
+        <source>Search functionality.</source>
+        <translation>Haku-toiminto.</translation>
     </message>
     <message>
-	<source>Cover page shows latest headlines.</source>
-	<translation>Kansisivu näyttää viimeisimmät otsikot.</translation>
+        <source>Cover page shows latest headlines.</source>
+        <translation>Kansisivu näyttää viimeisimmät otsikot.</translation>
     </message>
     <message>
-	<source>Pagination for getting more headlines.</source>
-	<translation>Mahdollisuus hakea lisää uutisotsikoita.</translation>
+        <source>Pagination for getting more headlines.</source>
+        <translation>Mahdollisuus hakea lisää uutisotsikoita.</translation>
     </message>
     <message>
-	<source>News sources from different regions which are supported by High.fi.</source>
-	<translation>Uutislähteitä nyt eri kielialueilta, joita High.fi tarjoaa.</translation>
+        <source>News sources from different regions which are supported by High.fi.</source>
+        <translation>Uutislähteitä nyt eri kielialueilta, joita High.fi tarjoaa.</translation>
     </message>
     <message>
-	<source>Add news item to favorites.</source>
-	<translation>Lisää uutisotsikko suosikkeihin.</translation>
+        <source>Add news item to favorites.</source>
+        <translation>Lisää uutisotsikko suosikkeihin.</translation>
     </message>
     <message>
-	<source>Reset application settings and favorites.</source>
-	<translation>Tyhjennä asetukset ja suosikki-lista.</translation>
+        <source>Reset application settings and favorites.</source>
+        <translation>Tyhjennä asetukset ja suosikki-lista.</translation>
     </message>
     <message>
-	<source>Settings are saved automatically.</source>
-	<translation>Asetukset tallennetaan automaattisesti.</translation>
+        <source>Settings are saved automatically.</source>
+        <translation>Asetukset tallennetaan automaattisesti.</translation>
     </message>
     <message>
-	<source>Show descriptions and use mobile links.</source>
-	<translation>Mahdollisuus näyttää lyhyt kuvaus otsikon alla ja käyttää mobiili-osoitteita.</translation>
+        <source>Show descriptions and use mobile links.</source>
+        <translation>Mahdollisuus näyttää lyhyt kuvaus otsikon alla ja käyttää mobiili-osoitteita.</translation>
     </message>
     <message>
-	<source>Copy link to clipboard.</source>
-	<translation>Kopioi osoite leikepöydälle.</translation>
+        <source>Copy link to clipboard.</source>
+        <translation>Kopioi osoite leikepöydälle.</translation>
     </message>
     <message>
-	<source>Filter shown news categories.</source>
-	<translation>Rajaa näytettäviä uutiskategorioita.</translation>
+        <source>Filter shown news categories.</source>
+        <translation>Rajaa näytettäviä uutiskategorioita.</translation>
     </message>
     <message>
-	<source>Added sub categories to category selection.</source>
-	<translation>Lisätty kategorialistaan alikategoriat.</translation>
+        <source>Added sub categories to category selection.</source>
+        <translation>Lisätty kategorialistaan alikategoriat.</translation>
     </message>
 </context>
 <context>
     <name>CoverPage</name>
     <message>
-	<source>Refreshing</source>
-	<translation>Päivitetään</translation>
+        <source>Refreshing</source>
+        <translation>Päivitetään</translation>
     </message>
     <message>
-	<source>updated</source>
-	<translation>päivitetty</translation>
+        <source>updated</source>
+        <translation>päivitetty</translation>
     </message>
 </context>
 <context>
     <name>FavoritesPage</name>
     <message>
-	<source>Favorites</source>
-	<translation>Suosikit</translation>
+        <source>Favorites</source>
+        <translation>Suosikit</translation>
     </message>
 </context>
 <context>
     <name>FeedItemContextMenu</name>
     <message>
-	<source>Copy link to clipboard</source>
-	<translation>Kopioi linkki leikepöydälle</translation>
+        <source>Copy link to clipboard</source>
+        <translation>Kopioi linkki leikepöydälle</translation>
     </message>
     <message>
-	<source>Open link in browser</source>
-	<translation>Avaa linkki selaimessa</translation>
+        <source>Open link in browser</source>
+        <translation>Avaa linkki selaimessa</translation>
     </message>
     <message>
-	<source>Launching browser.</source>
-	<translation>Avataan selainta.</translation>
+        <source>Launching browser.</source>
+        <translation>Avataan selainta.</translation>
     </message>
     <message>
-	<source>Add to favorites</source>
-	<translation>Lisää suosikkeihin</translation>
+        <source>Add to favorites</source>
+        <translation>Lisää suosikkeihin</translation>
     </message>
     <message>
-	<source>Link added to favorites</source>
-	<translation>Linkki lisätty suosikkeihin</translation>
+        <source>Link added to favorites</source>
+        <translation>Linkki lisätty suosikkeihin</translation>
     </message>
     <message>
-	<source>Remove from favorites</source>
-	<translation>Poista suosikeista</translation>
+        <source>Remove from favorites</source>
+        <translation>Poista suosikeista</translation>
     </message>
     <message>
-	<source>Favorite removed</source>
-	<translation>Linkki poistettu suosikeista</translation>
+        <source>Favorite removed</source>
+        <translation>Linkki poistettu suosikeista</translation>
     </message>
 </context>
 <context>
     <name>FeedsFilterView</name>
     <message>
-	<source>Hide categories</source>
-	<translation>Piilota kategoria</translation>
+        <source>Hide categories</source>
+        <translation>Piilota kategoria</translation>
     </message>
 </context>
 <context>
     <name>FeedsView</name>
     <message>
-	<source>Select favorite categories</source>
-	<translation>Valitse suosikkikategoriat</translation>
+        <source>Select favorite categories</source>
+        <translation>Valitse suosikkikategoriat</translation>
     </message>
 </context>
 <context>
     <name>MainPage</name>
     <message>
-	<source>About</source>
-	<translation>Tietoja</translation>
+        <source>About</source>
+        <translation>Tietoja</translation>
     </message>
     <message>
-	<source>Refresh</source>
-	<translation>Päivitä</translation>
+        <source>Refresh</source>
+        <translation>Päivitä</translation>
     </message>
     <message>
-	<source>Pull down to refresh</source>
-	<translation>Vedä alas päivittääksesi</translation>
+        <source>Pull down to refresh</source>
+        <translation>Vedä alas päivittääksesi</translation>
     </message>
     <message>
-	<source>Load more</source>
-	<translation>Lataa lisää</translation>
+        <source>Load more</source>
+        <translation>Lataa lisää</translation>
     </message>
     <message>
-	<source>Settings</source>
-	<translation>Asetukset</translation>
+        <source>Settings</source>
+        <translation>Asetukset</translation>
     </message>
     <message>
-	<source>powered by high.fi</source>
-	<translation>voimanlähteenä high.fi</translation>
+        <source>powered by high.fi</source>
+        <translation>voimanlähteenä high.fi</translation>
     </message>
     <message>
-	<source>Search...</source>
-	<translation>Hae</translation>
+        <source>Search...</source>
+        <translation>Hae</translation>
     </message>
     <message>
-	<source>No results</source>
-	<translation>Ei hakutuloksia</translation>
+        <source>No results</source>
+        <translation>Ei hakutuloksia</translation>
     </message>
     <message>
-	<source>Search</source>
-	<translation>Haku</translation>
+        <source>Search</source>
+        <translation>Haku</translation>
     </message>
     <message>
-	<source>No search results found</source>
-	<translation>Haku ei tuottanut yhtään osumaa</translation>
+        <source>No search results found</source>
+        <translation>Haku ei tuottanut yhtään osumaa</translation>
     </message>
     <message>
-	<source>Nothing found for the given search term. Try again with different search?</source>
-	<translation>Haetulla termillä ei löytynyt yhtään hakutulosta. Yritä uudelleen eri hakuehdolla?</translation>
+        <source>Nothing found for the given search term. Try again with different search?</source>
+        <translation>Haetulla termillä ei löytynyt yhtään hakutulosta. Yritä uudelleen eri hakuehdolla?</translation>
     </message>
     <message>
-	<source>Favorites</source>
-	<translation>Suosikit</translation>
+        <source>Favorites</source>
+        <translation>Suosikit</translation>
     </message>
 </context>
 <context>
     <name>SettingsPage</name>
     <message>
-	<source>Settings</source>
-	<translation>Asetukset</translation>
+        <source>Settings</source>
+        <translation>Asetukset</translation>
     </message>
     <message>
-	<source>Categories</source>
-	<translation>Kategoriat</translation>
+        <source>Categories</source>
+        <translation>Kategoriat</translation>
     </message>
     <message>
-	<source>Filter</source>
-	<translation>Rajaa</translation>
+        <source>Filter</source>
+        <translation>Rajaa</translation>
     </message>
 </context>
 <context>
     <name>SettingsView</name>
     <message>
-	<source>Settings</source>
-	<translation>Asetukset</translation>
+        <source>Settings</source>
+        <translation>Asetukset</translation>
     </message>
     <message>
-	<source>Show description</source>
-	<translation>Näytä lyhyt kuvaus</translation>
+        <source>Show description</source>
+        <translation>Näytä lyhyt kuvaus</translation>
     </message>
     <message>
-	<source>Use mobile optimized URLs</source>
-	<translation>Käytä mobiili-sivustoja</translation>
+        <source>Use mobile optimized URLs</source>
+        <translation>Käytä mobiili-sivustoja</translation>
     </message>
     <message>
-	<source>Region for categories</source>
-	<translation>Uutissyötteiden alueasetus</translation>
+        <source>Region for categories</source>
+        <translation>Uutissyötteiden alueasetus</translation>
     </message>
     <message>
-	<source>Show synopsis or abbreviation of the news story if available.</source>
-	<translation>Näytä tiivistelmä tai ote uutisesta, jos saatavilla.</translation>
+        <source>Show synopsis or abbreviation of the news story if available.</source>
+        <translation>Näytä tiivistelmä tai ote uutisesta, jos saatavilla.</translation>
     </message>
     <message>
-	<source>Use mobile optimized address for the story if available otherwise use normal address.</source>
-	<translation>Käytä mobiiliosoitteita uutisen avaamisessa, jos mahdollista, muuten normaalia osoitetta.</translation>
+        <source>Use mobile optimized address for the story if available otherwise use normal address.</source>
+        <translation>Käytä mobiiliosoitteita uutisen avaamisessa, jos mahdollista, muuten normaalia osoitetta.</translation>
     </message>
     <message>
-	<source>Select news portal&apos;s region for categories.</source>
-	<translation>&gt;Valitse alue, jonka uutissivustoista uutiset haetaan.</translation>
+        <source>Select news portal&apos;s region for categories.</source>
+        <translation>&gt;Valitse alue, jonka uutissivustoista uutiset haetaan.</translation>
     </message>
     <message>
-	<source>Resets all settings and removes all favorited news items.</source>
-	<translation>Poista kaikki asetukset ja suosikkeihin lisätyt linkit.</translation>
+        <source>Resets all settings and removes all favorited news items.</source>
+        <translation>Poista kaikki asetukset ja suosikkeihin lisätyt linkit.</translation>
     </message>
     <message>
-	<source>Reset Haikala</source>
-	<translation>Tyhjennä asetukset</translation>
+        <source>Reset Haikala</source>
+        <translation>Tyhjennä asetukset</translation>
     </message>
     <message>
-	<source>Resetting Haikala.</source>
-	<translation>Tyhjennetään asetuksia.</translation>
+        <source>Resetting Haikala.</source>
+        <translation>Tyhjennetään asetuksia.</translation>
     </message>
 </context>
 <context>
     <name>TabPanel</name>
     <message>
-	<source>Close</source>
-	<translation>Sulje</translation>
+        <source>Close</source>
+        <translation>Sulje</translation>
     </message>
 </context>
 <context>
     <name>WebPage</name>
     <message>
-	<source>Close web view</source>
-	<translation>Sulje web-näkymä</translation>
+        <source>Close web view</source>
+        <translation>Sulje web-näkymä</translation>
     </message>
     <message>
-	<source>Copy link to clipboard</source>
-	<translation>Kopioi linkki leikepöydälle</translation>
+        <source>Copy link to clipboard</source>
+        <translation>Kopioi linkki leikepöydälle</translation>
     </message>
     <message>
-	<source>Link</source>
-	<translation>Linkki</translation>
+        <source>Link</source>
+        <translation>Linkki</translation>
     </message>
     <message>
-	<source>copied to clipboard.</source>
-	<translation>kopioitu leikepöydälle.</translation>
+        <source>copied to clipboard.</source>
+        <translation>kopioitu leikepöydälle.</translation>
     </message>
     <message>
-	<source>Open in browser</source>
-	<translation>Avaa selaimessa</translation>
+        <source>Open in browser</source>
+        <translation>Avaa selaimessa</translation>
     </message>
 </context>
 <context>
     <name>main</name>
     <message>
-	<source>Error with %1:
+        <source>Error with %1:
 %2</source>
-	<translation>Virhe %1: %2</translation>
+        <translation>Virhe %1: %2</translation>
     </message>
     <message>
-	<source>Unknown error with code %1</source>
-	<translation>Tuntematon virhe koodilla %1</translation>
+        <source>Unknown error with code %1</source>
+        <translation>Tuntematon virhe koodilla %1</translation>
     </message>
 </context>
 <context>
     <name>utils</name>
     <message>
-	<source>Now</source>
-	<translation>Nyt</translation>
+        <source>Now</source>
+        <translation>Nyt</translation>
     </message>
     <message>
-	<source>Just now</source>
-	<translation>Juuri nyt</translation>
+        <source>Just now</source>
+        <translation>Juuri nyt</translation>
     </message>
     <message>
-	<source>&lt; 5 minutes</source>
-	<translation>&lt; 5 minuuttia</translation>
+        <source>&lt; 5 minutes</source>
+        <translation>&lt; 5 minuuttia</translation>
     </message>
     <message>
-	<source>&lt; 15 minutes</source>
-	<translation>&lt; 15 minuuttia</translation>
+        <source>&lt; 15 minutes</source>
+        <translation>&lt; 15 minuuttia</translation>
     </message>
     <message>
-	<source>&lt; 30 minutes</source>
-	<translation>&lt; 30 minuuttia</translation>
+        <source>&lt; 30 minutes</source>
+        <translation>&lt; 30 minuuttia</translation>
     </message>
     <message>
-	<source>&lt; 45 minutes</source>
-	<translation>&lt; 45 minuuttia</translation>
+        <source>&lt; 45 minutes</source>
+        <translation>&lt; 45 minuuttia</translation>
     </message>
     <message numerus="yes">
-	<source>%n hour(s)</source>
-	<translation>
-	    <numerusform>%n tunti(a)</numerusform>
-	    <numerusform></numerusform>
-	</translation>
+        <source>%n hour(s)</source>
+        <translation>
+            <numerusform>%n tunti(a)</numerusform>
+            <numerusform></numerusform>
+        </translation>
     </message>
     <message>
-	<source>Yesterday %1</source>
-	<translation>Eilen %1</translation>
+        <source>Yesterday %1</source>
+        <translation>Eilen %1</translation>
     </message>
 </context>
 </TS>
